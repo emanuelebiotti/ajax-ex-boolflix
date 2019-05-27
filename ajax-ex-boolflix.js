bf85c6dc2c867cf517e979c45d67a1b75e5e4e48
emanuele <emanuele.biotti@gmail.com>
2019-05-27 16:58:30 +0200

    prima impostazione chiamata ajax

--- ajax-ex-boolflix.js
+++ ajax-ex-boolflix.js
@@ -3,26 +3,19 @@ $(document).ready(function(){
   var source = $("#schedafilm").html();
   var template = Handlebars.compile(source);
 
-  var filmcercato = $('.cerca').text();
-
-
 $('button').click(function(){
-  console.log(filmcercato);
 
-
-
-  var url_base = 'https://api.themoviedb.org/3';
 
   $.ajax({
-    'url': 'https://api.themoviedb.org/3',
+    'url': 'https://api.themoviedb.org/3/search/movie',
     'method': 'GET',
     'data': {
       'api_key': 'eec20b4c5f03950e27efaf1ac357366d',
-      'query': '',
-      'title': '',
-      'language': '',
+      'query': $('.cerca').val()
     },
-    'success': function(data){
+    'success': function(res){
+      console.log(res);
+      console.log(res.results);
 
       var dettaglifilm = {
         'Titolo': '',
@@ -30,6 +23,15 @@ $('button').click(function(){
         'Lingua': '',
         'Voto': ''
       }
+
+      for (var i = 0; i < res.results.length; i++) {
+        console.log(res.results[i].title);
+        console.log($('.cerca').val());
+
+        if (res.results[i].title.includes($('.cerca').val())) {
+          dettaglifilm.Titolo = res.results[i].title;
+        }
+      };
 
       var html = template(dettaglifilm);
 
