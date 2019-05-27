0f142e35671d6966bece54bf94b2dd4ae616863c
emanuele <emanuele.biotti@gmail.com>
2019-05-27 17:16:28 +0200

    dettagli layout

--- ajax-ex-boolflix.js
+++ ajax-ex-boolflix.js
@@ -3,52 +3,49 @@ $(document).ready(function(){
   var source = $("#schedafilm").html();
   var template = Handlebars.compile(source);
 
-$('button').click(function(){
+  $('button').click(function(){
 
+    $.ajax({
+      'url': 'https://api.themoviedb.org/3/search/movie',
+      'method': 'GET',
+      'data': {
+        'api_key': 'eec20b4c5f03950e27efaf1ac357366d',
+        'query': $('.cerca').val()
+      },
+      'success': function(res){
+        console.log(res);
+        console.log(res.results);
 
-  $.ajax({
-    'url': 'https://api.themoviedb.org/3/search/movie',
-    'method': 'GET',
-    'data': {
-      'api_key': 'eec20b4c5f03950e27efaf1ac357366d',
-      'query': $('.cerca').val()
-    },
-    'success': function(res){
-      console.log(res);
-      console.log(res.results);
-
-      var dettaglifilm = {
-        'Titolo': '',
-        'Titolo_originale': '' ,
-        'Lingua': '',
-        'Voto': ''
-      }
-
-      for (var i = 0; i < res.results.length; i++) {
-        console.log(res.results[i].title);
-        console.log($('.cerca').val());
-
-        if (res.results[i].title.includes($('.cerca').val())) {
-          dettaglifilm.Titolo = res.results[i].title;
-          dettaglifilm.Titolo_originale = res.results[i].original_title;
-          dettaglifilm.Lingua = res.results[i].original_language;
-          dettaglifilm.Voto = res.results[i].vote_average;
+        var dettaglifilm = {
+          'Titolo': '',
+          'Titolo_originale': '' ,
+          'Lingua': '',
+          'Voto': ''
         }
-      };
 
-      var html = template(dettaglifilm);
+        for (var i = 0; i < res.results.length; i++) {
+          console.log(res.results[i].title);
+          console.log($('.cerca').val());
 
-      $('.ricerca').append(html);
+          if (res.results[i].title.includes($('.cerca').val())) {
+            dettaglifilm.Titolo = res.results[i].title;
+            dettaglifilm.Titolo_originale = res.results[i].original_title;
+            dettaglifilm.Lingua = res.results[i].original_language;
+            dettaglifilm.Voto = res.results[i].vote_average;
+
+            var html = template(dettaglifilm);
+
+            $('.ricerca').append(html);
+          }
+        };
+      },
+      'error': function() {
+        alert('errore');
+      }
+    })
 
 
-    },
-    'error': function() {
-      alert('errore');
-    }
-  })
-
-
-});
+  });
 
 
 
